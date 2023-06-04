2751de5361c9e3849a1cad36a39455f3b8fbf691
Melvin Soldia <melvinsoldia@gmail.com>
2023-06-04 20:08:17 +0800

    textarea autoresize

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -1,17 +1,49 @@
 import { useSession } from "next-auth/react";
 import { Button } from "./Button";
 import { ProfileImage } from "./ProfileImage";
+import { useCallback, useLayoutEffect, useRef, useState } from "react";
+import type { Session } from "next-auth";
+
+const updateTextareaSize = (textarea?: HTMLTextAreaElement) => {
+  if (!textarea) return;
+  textarea.style.height = "0";
+  textarea.style.height = `${textarea.scrollHeight}px`;
+};
 
 export const NewTweetForm = () => {
   const { data: sessionData } = useSession();
 
   if (!sessionData?.user) return null;
 
+  // to fix the useLayoutEffect warning
+  return <Form user={sessionData.user} />;
+};
+
+interface Props {
+  user: Session["user"];
+}
+export const Form = ({ user }: Props) => {
+  const [inputValue, setInputValue] = useState("");
+  const textareaRef = useRef<HTMLTextAreaElement>();
+
+  const inputRef = useCallback((textarea: HTMLTextAreaElement) => {
+    updateTextareaSize(textarea);
+    textareaRef.current = textarea;
+  }, []);
+
+  useLayoutEffect(() => {
+    updateTextareaSize(textareaRef.current);
+  }, [inputValue]);
+
   return (
     <form className="flex flex-col gap-2 border-b px-4 py-2">
       <div className="flex gap-4">
-        <ProfileImage src={sessionData.user.image} />
+        <ProfileImage src={user.image} />
         <textarea
+          ref={inputRef}
+          style={{ height: 0 }}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
           className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
           placeholder="What's happening?"
         />
