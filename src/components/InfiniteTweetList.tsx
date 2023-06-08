af4c0e75ca4cbade97eb13acb69272a98042ac69
Melvin Soldia <melvinsoldia@gmail.com>
2023-06-08 18:46:13 +0800

    HeartButton

--- src/components/InfiniteTweetList.tsx
+++ src/components/InfiniteTweetList.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { ProfileImage } from "./ProfileImage";
+import { useSession } from "next-auth/react";
+import { VscHeart, VscHeartFilled } from "react-icons/vsc";
 
 interface Tweet {
   id: string;
@@ -79,7 +81,46 @@ const TweetCard = ({
           <span className="text-gray-500">{dateTimeFormatter.format()}</span>
         </div>
         <p className="whitespace-pre-wrap">{content}</p>
+        <HeartButton likedByMe={likedByMe} likeCount={likeCount} />
       </div>
     </li>
+  );
+};
+
+interface HeartButtonProps {
+  likedByMe: boolean;
+  likeCount: number;
+}
+
+const HeartButton = ({ likedByMe, likeCount }: HeartButtonProps) => {
+  const { status } = useSession();
+  const HeartIcon = likedByMe ? VscHeartFilled : VscHeart;
+
+  if (status !== "authenticated") {
+    return (
+      <div className="mb-1 mt-1 flex items-center gap-3 self-start text-gray-500">
+        <HeartIcon />
+        <span>{likeCount}</span>
+      </div>
+    );
+  }
+
+  return (
+    <button
+      className={`group flex items-center gap-1 self-start transition-colors duration-200 ${
+        likedByMe
+          ? "text-red-500"
+          : "text-gray-500 hover:text-red-500 focus-visible:text-red-500"
+      }`}
+    >
+      <HeartIcon
+        className={`transition-colors duration-200 ${
+          likedByMe
+            ? "fill-red-500"
+            : "fill-gray-500 group-hover:fill-red-500 group-focus-visible:fill-red-500"
+        }`}
+      />
+      <span>{likeCount}</span>
+    </button>
   );
 };
